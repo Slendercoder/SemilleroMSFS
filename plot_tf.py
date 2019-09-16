642db2fda12041b69054b82ebfe0872f73f3a9a2
carlos <carlosedu.alvarez@urosario.edu.co>
2019-09-16 17:22:40 -0500

    Carlos subió gráfica .py

---
+++ plot_tf.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+
+
+data = []
+Inp = open("tallaFinita.csv", "r")
+count = 0
+for line in Inp:
+    v = list(map(int, line.split(',')))
+    print(v)
+    if count > 0: 
+        data.append(v)
+    count += 1
+Inp.close()
+
+x = [i for i in range(len(data[0]))]
+
+print(data)
+
+fig = plt.figure()
+
+ax = fig.add_subplot(1,1,1)
+
+for i in range(8):
+    ax.plot(x, data[i])
+
+plt.show()
