8101fa18f22706741a5b3d5edd631f8be9af04a4
Slendercoder <andradeed78@gmail.com>
2019-10-21 17:50:21 -0500

    Inicializando ElFarol hasta guardar

---
+++ Codigos/ElFarol.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import random as rd
+import numpy as np
+import matplotlib.pyplot as plt
+
+class agente:
+    def __init__(self, estados, scores, estrategias):
+        self.estado = estados # lista
+        self.score = scores # lista
+        self.estrategia = estrategias # lista
+
+def crear_agentes_aleatorios(Num_agentes):
+    Agentes = []
+    for i in range(Num_agentes):
+        Agentes.append(agente([rd.randint(0,1)], [0], [rd.randint(0,7)]))
+
+    return Agentes
+
+def crear_politicas():
+    politicas = [
+    {(0,0): 0, (1,1): 0, (1, -1): 0},
+    {(0,0): 0, (1,1): 0, (1, -1): 1},
+    {(0,0): 0, (1,1): 1, (1, -1): 0},
+    {(0,0): 0, (1,1): 1, (1, -1): 1},
+    {(0,0): 1, (1,1): 0, (1, -1): 0},
+    {(0,0): 1, (1,1): 0, (1, -1): 1},
+    {(0,0): 1, (1,1): 1, (1, -1): 0},
+    {(0,0): 1, (1,1): 1, (1, -1): 1},
+    ]
+    return politicas
+
+def crear_red(Agentes):
+    red = {}
+    for i in range(len(Agentes)):
+        red[i] = [x for x in range(len(Agentes)) if x!=i]
+
+    return red
+
+def calcula_medio(agentes):
+    a = [x.estado for x in agentes]
+    return np.sum(a)/len(a)
+
+def juega_ronda(Agentes, politicas):
+    X = calcula_medio(Agentes)
+    for a in Agentes:
+        if a.estado[-1] == 1:
+            if X > 0.5:
+                a.score.append(-1)
+            else:
+                a.score.append(1)
+        else:
+            a.score.append(0)
+
+        polit = politicas[a.estrategia[-1]]
+        a.estado.append(polit[(a.estado[-1], a.score[-1])])
+
+    return Agentes
+
+def agentes_aprenden(Agentes, Red):
+    return Agentes
+
+def crea_dataframe_agentes(Agentes, Num_iteraciones):
+    print(Agentes)
+    agente = []
+    ronda = []
+    estado = []
+    puntaje = []
+    politica = []
+    for i in range(len(Agentes)):
+        for r in range(Num_iteraciones):
+            agente.append(i)
+            ronda.append(r)
+            estado.append(Agentes[i].estado[r])
+            puntaje.append(Agentes[i].score[r])
+            politica.append(Agentes[i].estrategia[r])
+
+    data = pd.DataFrame.from_dict(\
+    {\
+    'Agente': agente,\
+    'Ronda': ronda,\
+    'Estado': estado,\
+    'Puntaje': puntaje,\
+    'Politica': politica\
+    })
+
+def guardar(dataFrame, archivo):
+    dataFrame.pd.to_csv(archivo, index=False)
+
+def cargar(archivo):
+    data = pd.read_csv(archivo)
+
+# SIMULACION
+#
+# ANALISIS_ESTRATEGIAS
+#
+# ANALISIS_ASISTENCIA
+#
+# ANALISIS_AGENTES
---
+++ Codigos/main.py
@@ -0,0 +1,38 @@
+import ElFarol as EF
+
+Num_agentes = 2
+Num_iteraciones = 10
+TIPO_RED = 0 # COMPLETA
+
+agentes = EF.crear_agentes_aleatorios(Num_agentes)
+print(agentes)
+
+politicas = EF.crear_politicas()
+
+# CARLOS
+red = EF.crear_red(agentes)
+
+# YA
+agentes = EF.juega_ronda(agentes, politicas)
+print(agentes)
+
+# ESTEBAN
+agentes = EF.agentes_aprenden(agentes, red)
+
+# # MARIA JOSE
+# SIMULACION(Num_Iteraciones)
+#     RONDA
+#     APRENDIZAJE
+
+data = EF.crea_dataframe_agentes(agentes, Num_iteraciones)
+
+EF.guardar(data, 'agentes.csv')
+
+# # EDGAR
+# HISTOGRAMA_USO, GRAFICA_USO_VS_TIEMPO, GRAFICA_PUNTAJE = ANALISIS_ESTRATEGIAS(AGENTES)
+#
+# # ROJAS
+# GRAFICA_ASISTENTES_BAR_VS_RONDA = ANALISIS_ASISTENCIA(AGENTES)
+#
+# # MIGUEL
+# GRAFICA_PUNTAJE_VS_RONDA, GRAFICA_ESTRATEGIA_VS_RONDA = ANALISIS_AGENTES(AGENTES)
