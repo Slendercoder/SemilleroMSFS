629227c8eb3720f672b2214e5575cbde4ac91af1
Slendercoder <andradeed78@gmail.com>
2019-12-12 14:31:19 -0500

    Dibujando promedio frecuencia de uso

--- Codigos/graficas.py
+++ Codigos/graficas.py
@@ -9,16 +9,65 @@ def bins_labels(bins, **kwargs):
 
 data = pd.read_csv('agentes.csv')
 
-data = data.sort_values(['Ronda', 'Politica'], ascending=[True, True]).reset_index(drop=True)
-print(data[:10])
+# Encuentra la frecuencia promedio de uso de las politicas por ronda
+data = data.sort_values(['Identificador', 'Politica', 'Ronda'], ascending=[True, True, True])
+data['Freq1'] = data.groupby(['Identificador', 'Politica', 'Ronda'])['Politica'].transform('count')
+data['Sum1'] = data.groupby(['Identificador', 'Ronda'])['Politica'].transform('size')
+data['Freq1'] = data['Freq1'] / data['Sum1']
+del data['Sum1']
+data = data.sort_values(['Identificador', 'Ronda', 'Politica'], ascending=[True, True, True])
+print(data[['Identificador', 'Ronda', 'Politica', 'Freq1']][:15])
+data['AV_Freq1'] = data.groupby(['Politica', 'Ronda'])['Freq1'].transform('mean')
+data['AV_Freq1_cuad'] = np.std(data['Freq1'])
+# data['AV_Freq1_cuad'] = data.apply(lambda x: np.sqrt(np.mean(x['Freq1']*x['Freq1']) - (x['AV_Freq1']*x['AV_Freq1'])), axis=1)
 
-# bins = range(8)
-# ax = data.hist('Politica', align='mid', bins=bins, grid=False)
-# bins_labels(bins, fontsize=16)
+ # - data['AV_Freq1']*data['AV_Freq1']).apply(lambda x: np.sqrt(x))
+for key, grp in data.groupby('Politica'):
+    print(grp[['Ronda', 'Politica', 'Freq1', 'AV_Freq1', 'AV_Freq1_cuad']][:10])
+
+# # Crea plot de uso promedio de las politicas por ronda
+rondas = list(data.Ronda.unique())
+# print('Rondas', rondas)
+politicas = data.Politica.unique()
+lista_colores = ['#d55e00', '#cc79a7', '#0072b2', '#f0e442', '#009e73', '#004949', '#db6d00', '#924900']
+colores = {}
+for p in range(len(politicas)):
+    colores[politicas[p]] = lista_colores[p]
+# print(colores)
+fig, ax = plt.subplots()
+for key, grp in data.groupby('Politica'):
+    # print('Trabajando con politica', key)
+    av_freq = []
+    for Key, Grp in grp.groupby('Ronda'):
+        av_freq.append(list(Grp['AV_Freq1'].unique())[0])
+    # print(av_freq)
+    # print(colores[key])
+    ax.plot(rondas, av_freq, label=key, color=colores[key])
+ax.set_xlabel('Ronda', fontsize = 14)
+ax.set_ylabel('Promedio Frecuencia uso', fontsize = 14)
+ax.set_title(u'Frecuencia promedio de uso de las políticas')
+print('Dibujando')
+plt.legend(loc='best')
+plt.show()
+
+
+# # Crea histograma de uso de las politicas
+# fig, ax = plt.subplots()
+# bins = range(9)
+# bins_labels(bins, fontsize=12)
+# grp.hist('Politica', align='mid', bins=bins, grid=False, ax=ax)
+# ax.set_xlabel(u'Política', fontsize = 14)
+# ax.set_title(u'Frecuencia total de uso de las políticas')
 # plt.show()
 
-data['Freq1'] = data.groupby(['Politica', 'Ronda'])['Politica'].transform('count')
-print(data[:10])
+# # Encuentra el puntaje acumulado por politica
+# data = data.sort_values(['Identificador', 'Politica'], ascending=[True, True]).reset_index(drop=True)
+# data['AcScorPol'] = data.groupby(['Identificador', 'Politica'])['Puntaje'].transform('sum')
+# # print(data[['Politica', 'AcScorPol']][:100])
+#
+# # Encuentra el puntaje acumulado por politica por ronda
+# data['AcScorPolRond'] = data.groupby(['Identificador', 'Politica', 'Ronda'])['Puntaje'].transform('sum')
+# # print(data[['Politica', 'Ronda', 'AcScorPolRond']][:10])
 
 # pols = data.Politica.unique()
 # print(pols)
@@ -38,18 +87,11 @@ print(data[:10])
 #
 # ax = grp.plot(x='Ronda', y='Politica')
 
-data = data.sort_values(['Politica'], ascending=[True]).reset_index(drop=True)
-data['AcScorPol'] = data.groupby('Politica')['Puntaje'].transform('sum')
-print(data[['Politica', 'AcScorPol']][:100])
+# ax = data.plot.bar(x='Politica', y='AcScorPol')
 
-ax = data.plot.bar(x='Politica', y='AcScorPol')
-
-data['AcScorPolRond'] = data.groupby(['Politica', 'Ronda'])['Puntaje'].transform('sum')
-print(data[['Politica', 'Ronda', 'AcScorPolRond']][:10])
-#
 # ax = data.groupby('Politica').plot(x='Ronda', y='AcScorPolRond', marker='o')
 
-plt.show()
+# plt.show()
 
 # # EDGAR
 # HISTOGRAMA_USO, GRAFICA_USO_VS_TIEMPO, GRAFICA_PUNTAJE = ANALISIS_ESTRATEGIAS(AGENTES)
