433bc5e874bd6f0065aedc7f7304294e0034b7bb
Nirogu <nirogu@outlook.com>
2020-06-19 12:46:34 -0500

    Notebook analisis

--- Codigos/ElFarolFunciones.py
+++ Codigos/ElFarolFunciones.py
@@ -1,6 +1,15 @@
 import numpy as np
 import pandas as pd
 
+class agente:
+    def __init__(self, estados, scores, politicas, vecinos):
+        self.estado = estados # lista
+        self.score = scores # lista
+        self.politica = politicas # lista
+        self.vecinos = vecinos
+    def __str__(self):
+        return "E:{0}, S:{1}, P:{2}, V{3}".format(self.estado, self.score,self.politica,self.vecinos)
+
 def calcula_medio(agentes):
     a = [x.estado[-1] for x in agentes]
     return np.sum(a)/len(a)
@@ -111,10 +120,8 @@ def crea_dataframe_agentes(Agentes, Num_iteraciones, PARAMETROS, N):
 
 def guardar(dataFrame, archivo, inicial):
     # dataFrame.to_csv(archivo, index=False)
-
     with open(archivo, 'a') as f:
         dataFrame.to_csv(f, header=inicial, index=False)
-
     f.close()
 
 def cargar(archivo):
@@ -127,3 +134,66 @@ def encontrar_consistencia(politica, politica_lag):
     elif politica == politica_lag:
         return 1
     else: return 0
+
+def crear_agentes_aleatorios(Num_agentes):
+    Agentes = []
+    for i in range(Num_agentes):
+        Agentes.append(agente([rd.randint(0,1)], [], [rd.randint(0,7)], []))
+
+    X = calcula_medio(Agentes)
+    
+    for a in Agentes:
+        if a.estado[-1] == 1:
+            if X > 0.5:
+                a.score.append(-1)
+            else:
+                a.score.append(1)
+        else:
+            a.score.append(0)
+
+    return Agentes
+
+def crear_politicas():
+    politicas = [
+    {(0,0): 0, (1,1): 0, (1, -1): 0}, #0
+    {(0,0): 0, (1,1): 0, (1, -1): 1}, #1
+    {(0,0): 0, (1,1): 1, (1, -1): 0}, #2
+    {(0,0): 0, (1,1): 1, (1, -1): 1}, #3
+    {(0,0): 1, (1,1): 0, (1, -1): 0}, #4
+    {(0,0): 1, (1,1): 0, (1, -1): 1}, #5
+    {(0,0): 1, (1,1): 1, (1, -1): 0}, #6
+    {(0,0): 1, (1,1): 1, (1, -1): 1}, #7
+    ]
+    return politicas
+
+def agentes_aprenden(Agentes):
+    #Los agentes copian la politica del ganador de la Ronda
+    for agente in Agentes:
+        #print(Agentes.index(agente))
+        maximo=agente.score[-1]
+        maximo_vecino=Agentes.index(agente)
+        #print(agente.vecinos)
+        for index_vecino in agente.vecinos:
+            if((Agentes[index_vecino].score[-1])>(maximo)):
+                #print('Hay cambio')
+                #print('Puntaje anterior',maximo)
+                maximo=Agentes[index_vecino].score[-1]
+                #print('Puntaje anterior vecino',maximo)
+                maximo_vecino=index_vecino
+            #else:
+                #print('No hay cambio')
+        agente.politica.append(Agentes[maximo_vecino].politica[-1])
+    return Agentes
+
+def simulacion(Num_agentes, Num_iteraciones, UMBRAL, inicial, N, PARS):
+    agentes = crear_agentes_aleatorios(Num_agentes)
+    politicas = crear_politicas()
+    # Leyendo red de archivo
+    leer_red(agentes)
+    for i in range(Num_iteraciones):
+        agentes = juega_ronda(agentes, politicas, UMBRAL)
+        agentes = agentes_aprenden(agentes)
+    data = crea_dataframe_agentes(agentes, Num_iteraciones, PARS, N)
+    data['Politica_lag'] = data.groupby('Agente')['Politica'].transform('shift', 1)
+    data['Consistencia'] = data.apply(lambda x : encontrar_consistencia (x['Politica'], x['Politica_lag']), axis=1)
+    guardar(data, 'agentes.csv', inicial)
